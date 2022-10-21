0d29ee3fef6ac6581cf84e8c2cf16595ad1f85a3
Lucas Dell'Isola <dev@lucasdellisola.com.ar>
2022-10-21 12:15:57 -0300

    [Feature] Added functions to read and write csv files

--- api/src/main/java/ar/edu/itba/pod/csv/CsvHelper.java
+++ api/src/main/java/ar/edu/itba/pod/csv/CsvHelper.java
@@ -2,13 +2,17 @@ package ar.edu.itba.pod.csv;
 
 import ar.edu.itba.pod.models.Reading;
 import ar.edu.itba.pod.models.Sensor;
+import lombok.Cleanup;
 
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
 import java.util.List;
+import java.util.concurrent.Callable;
+import java.util.function.Function;
 
-public class CsvReader {
+public class CsvHelper {
     
     public static List<Sensor> parseSensorFile(String path) throws IOException {
         return Files.readAllLines(Path.of(path))
@@ -24,5 +28,20 @@ public class CsvReader {
                 .skip(1)
                 .map(Reading::new)
                 .toList();
+    }
+    
+    public static <T> void writeFile(String path, String header, List<T> a, Function<T,String> serialize) throws IOException {
+        @Cleanup var buffer = Files.newBufferedWriter(
+                Path.of(path),
+                StandardOpenOption.CREATE,
+                StandardOpenOption.WRITE
+        );
+
+        buffer.write(header);
+        for (var entry: a) {
+            buffer.newLine();
+            buffer.write(serialize.apply(entry));
+        }
+        
     }
 }
